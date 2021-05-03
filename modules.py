b0754afa03f815a282682671a340e59dba1ec230
Divya Sharma <45572873+devdroid19@users.noreply.github.com>
2021-05-03 00:55:05 -0500

    Hypothesis 3 Function added

--- modules.py
+++ modules.py
@@ -1,11 +1,19 @@
 import pandas as pd
 import plotly.express as px
 from numba import jit
+from datetime import datetime
+import numpy as np
 pd.options.plotting.backend = "plotly"
 
 
 def convert_lat_lon(x:str) -> tuple:
     return float(eval(x)['latitude']), float(eval(x)['longitude'])
+
+
+def was_time_ahead(check_date):
+    check_date = datetime.strptime(check_date, '%Y-%m-%d')
+    dst_selected_year = dst_data.loc[check_date.year].to_dict()
+    return dst_selected_year['Start Complete'] <= check_date < dst_selected_year['End Complete']
 
 
 @jit(forceobj=True)
@@ -61,14 +69,28 @@ def full_moon_finder(file_path: str):
     return moon_dates
 
 
-def dst_clean(file_path: str):
+def dst_clean(file_path: str, days_to_check):
     dst_data = pd.read_csv(file_path, dtype={'Year': 'str'}, index_col='Year')
     dst_data['Start Complete'] = pd.to_datetime(dst_data['Start'] + ' ' + dst_data.index.astype(str))
     dst_data['End Complete'] = pd.to_datetime(dst_data['End'] + ' ' + dst_data.index.astype(str))
-    return dst_data
+    was_time_ahead = np.vectorize(was_time_ahead)
+    was_dst_df = pd.DataFrame({'Date': days_to_check,
+                             'WasDST': was_time_ahead(days_to_check)
+                             })
+    return was_dst_df
 
 
 def race_clean(file_path: str):
     race_data = pd.read_csv(file_path, dtype={'Zip Code': 'str'})
     race_data = race_data[race_data['Total Population'] > 1000]
     return race_data
+
+
+def plot_daylight_crime_rate(crime_desc_list):
+    for crime_desc in crime_desc_list:
+        plot_data = crime_rates_dst[crime_rates_dst['Crime Code Description'] == crime_desc].melt().iloc[1:]
+        fig = plot_data.plot.bar(x='variable',
+                            y='value',
+                           title=crime_desc,
+                           labels={'variable':'Time','value':'Daily Crime Rate'})
+        fig.show()
